32759961ec12c198c61c726ad17a52f9590d41ea
nicklavers <nick.lavers@videoamp.com>
2016-10-19 13:44:48 -0700

    Added Bootstrap object. Generalized SparkProperties.load method. (#1)
    Added bootstrap object. Generalized SparkProperties.load method

---
+++ src/main/scala/bootstrap.scala
@@ -0,0 +1,89 @@
+import vamp.ammonium.setup.{ Setup, CodePreamble }
+import vamp.ammonium.setup.hocon.extractSetup
+
+import java.net.URL
+import com.typesafe.config.ConfigFactory
+import ammonite.api.{ Classpath, Eval }
+
+import better.files._
+import argonaut._, Argonaut._
+
+object bootstrap {
+  case class Jar(name: String, signature: String)
+  implicit def JarDecodeJson: CodecJson[Jar] = CodecJson.derive[Jar]
+
+  def apply(
+    masterIP: String,
+    port: Int = 8088,
+    jarsPath: String = "/var/tmp/ammonium/bootstrap",
+    silent: Boolean = true,
+    silentEval: Boolean = true
+  )(
+    implicit
+    classpath: Classpath,
+    eval: Eval
+  ): Unit = {
+    val logger: String => String = if (silent) identity else x => { println(x); x }
+
+    val jarsDir = jarsPath.toFile
+    jarsDir.createDirectories
+
+    val serverRoot = new URL("http", masterIP, port, "/")
+
+    val manifest: List[Jar] = new URL(serverRoot, "jars/MANIFEST.json")
+      .openStream
+      .reader
+      .buffered
+      .tokens
+      .mkString
+      .decodeOption[List[Jar]]
+      .getOrElse(throw new RuntimeException("Failed to parse manifest at " + serverRoot + "jars/MANIFEST.json"))
+
+    val in: Setup = extractSetup(ConfigFactory.parseURL(new URL(serverRoot, "setup/flint")))
+      .getOrElse(throw new RuntimeException("Failed to extract setup from " + serverRoot + "setup/flint"))
+
+    logger("Checking manifest and fetching missing jar files...")
+
+    val jarPaths: List[(String, Boolean)] = for {
+      Jar(name, signature) <- manifest
+      jarFile = jarsDir / signature / name
+    } yield {
+      if (!jarFile.exists) {
+        jarFile.createIfNotExists(asDirectory = false, createParents = true)
+        new URL(serverRoot, "jars/" + name).openStream > jarFile.newOutputStream
+        logger("Fetched from server:")
+      }
+      else {
+        logger("Found locally:")
+      }
+      (
+        logger(jarFile.toString),
+        jarFile.sha256.toLowerCase == signature.toLowerCase
+      )
+    }
+
+    jarPaths.collect{
+      case (jar, false) => jar
+    } match {
+      case Nil => ()
+      case ls => throw new RuntimeException(ls.mkString("Bad checksums found: ", ", ", ""))
+    }
+
+    val confURL = new URL(serverRoot, "conf/spark-defaults.conf")
+    val loadConf = "_root_.vamp.ammonium.SparkProperties.loadURL(\"" + confURL + "\")"
+    val setMaster = "sparkConf.setMaster(\"spark://" + masterIP + ":7077\")"
+    val extraPreamble = "\nConfigured using " + confURL
+
+    val out: Setup = in.copy(
+      classpathEntries = in.classpathEntries.map(_ ++ jarPaths.map(_._1)),
+      codePreambles = in.codePreambles.map(_.map{
+        case CodePreamble("Spark", code) =>
+          CodePreamble("Spark", loadConf +: code :+ setMaster)
+        case x => x
+      }),
+      preamble = in.preamble.map(_ + extraPreamble)
+    )
+
+    new vamp.ammonium.Setup(classpath, eval).init(out, silent, silentEval)
+  }
+}
--- src/main/scala/vamp/ammonium/SparkProperties.scala
+++ src/main/scala/vamp/ammonium/SparkProperties.scala
@@ -1,29 +1,31 @@
 package vamp.ammonium
 
+import java.net.URL
+import java.io.{ Reader, File => JFile }
+import java.util.Properties
+import scala.collection.JavaConverters._
+
+import better.files._
+
 object SparkProperties {
-  def load(filename: String): Unit =
-    getPropertiesFromFile(filename)
-      .filter { case (k, v) => k.startsWith("spark.") }
-      .foreach {
-        case (k, v) =>
-          sys.props.getOrElseUpdate(k, v)
-      }
+  def load(filename: String): Unit = loadFile(filename)
 
-  private def getPropertiesFromFile(filename: String): Map[String, String] = {
-    import java.io._
-    import java.util._
-    import scala.collection.JavaConversions._
+  def loadFile(filename: String): Unit = loadFile(filename.toFile)
+  def loadFile(file: JFile): Unit = loadFile(file.toScala)
+  def loadFile(file: File): Unit = load(file.newFileReader.autoClosed)
 
+  def loadURL(urlString: String): Unit = loadURL(new URL(urlString))
+  def loadURL(url: URL): Unit = load(url.openStream.reader.autoClosed)
+
+  private def load(inReader: ManagedResource[Reader]): Unit =
+    for {
+      (k, v) <- getProperties(inReader)
+      if k.startsWith("spark.")
+    } sys.props.getOrElseUpdate(k, v)
+
+  private def getProperties(inReader: ManagedResource[Reader]): Map[String, String] = {
     val properties = new Properties
-    val inReader = new InputStreamReader(new FileInputStream(filename), "UTF-8")
-
-    try {
-      properties.load(inReader)
-    }
-    finally {
-      inReader.close
-    }
-
-    properties.stringPropertyNames.map(k => (k, properties(k).trim)).toMap
+    inReader.foreach(reader => properties.load(reader))
+    properties.stringPropertyNames.asScala.map(k => (k, properties.getProperty(k).trim)).toMap
   }
 }
